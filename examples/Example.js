2da2b8f9cf091991586231b8048c18ad7092d058
Andrew Durber <aidurber@gmail.com>
2017-12-25 16:44:02 +0000

    test(filter): Added filter tests

--- examples/Example.js
+++ examples/Example.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import Picky from '../src/index';
+const oneToOneThousand = [];
 
+for (var i = 1; i <= 1000; i++) {
+  oneToOneThousand.push(i);
+}
 class Example extends React.Component {
   constructor(props) {
     super(props);
@@ -25,33 +29,36 @@ class Example extends React.Component {
   render() {
     return (
       <div>
+        {
+          // <div>
+          //   <div style={{ width: '50%', float: 'left' }}>
+          //     <Picky
+          //       value={this.state.value}
+          //       options={[1, 2, 3, 4]}
+          //       onChange={this.selectOption}
+          //     />
+          //   </div>
+          //   <div style={{ width: '50%', float: 'left' }}>
+          //     <Picky
+          //       value={this.state.arrayValue}
+          //       options={[1, 2, 3, 4]}
+          //       onChange={this.selectMultipleOption}
+          //       multiple
+          //     />
+          //   </div>
+          // </div>
+          // <br />
+        }
         <div>
           <div style={{ width: '50%', float: 'left' }}>
             <Picky
-              value={this.state.value}
-              options={[1, 2, 3, 4]}
-              onChange={this.selectOption}
-            />
-          </div>
-          <div style={{ width: '50%', float: 'left' }}>
-            <Picky
               value={this.state.arrayValue}
-              options={[1, 2, 3, 4]}
-              onChange={this.selectMultipleOption}
-              multiple
-            />
-          </div>
-        </div>
-        <br />
-        <div>
-          <div style={{ width: '50%', float: 'left' }}>
-            <Picky
-              value={this.state.arrayValue}
-              options={[1, 2, 3, 4]}
+              options={oneToOneThousand}
               onChange={this.selectMultipleOption}
               open={false}
               multiple={true}
               includeSelectAll={true}
+              includeFilter={true}
             />
           </div>
         </div>
--- src/Filter.js
+++ src/Filter.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class Filter extends Component {
   render() {
-    return <h1>Filter</h1>;
+    return (
+      <div className="picky__filter">
+        <input
+          type="text"
+          className="picky__filter__input"
+          placeholder="Filter..."
+          onChange={event => this.props.onFilterChange(event.target.value)}
+        />
+      </div>
+    );
   }
 }
+
+Filter.propTypes = {
+  onFilterChange: PropTypes.func.isRequired
+};
 
 export default Filter;
