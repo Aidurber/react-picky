83f6a7826435f71fd9c99b52724e3f018c8fbf51
Andrew Durber <aidurber@gmail.com>
2017-12-26 18:46:26 +0000

    refactor(Placeholder): Make placeholder pure component

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -1,43 +1,62 @@
 // NEEDS REFACTOR
 import React from 'react';
 import PropTypes from 'prop-types';
-const Placeholder = ({
-  placeholder,
-  value,
-  numberDisplayed,
-  multiple,
-  valueKey,
-  labelKey
-}) => {
-  let message = '';
-  // Show placeholder if no value
+class Placeholder extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  if (
-    value === null ||
-    value === undefined ||
-    (Array.isArray(value) && !value.length)
-  ) {
-    message = placeholder;
-  } else if (Array.isArray(value)) {
-    // If type is array and values length less than number displayed
-    // join the values
-    if (multiple) {
-      if (value.length <= numberDisplayed) {
-        message = value
-          .map(opt => {
-            if (
-              typeof opt === 'object' &&
-              opt.hasOwnProperty(valueKey) &&
-              opt.hasOwnProperty(labelKey)
-            ) {
-              return opt[labelKey];
-            }
-            return opt;
-          })
-          .join(', ');
+  render() {
+    const {
+      props: {
+        placeholder,
+        value,
+        numberDisplayed,
+        multiple,
+        valueKey,
+        labelKey
+      }
+    } = this;
+
+    let message = '';
+    // Show placeholder if no value
+
+    if (
+      value === null ||
+      value === undefined ||
+      (Array.isArray(value) && !value.length)
+    ) {
+      message = placeholder;
+    } else if (Array.isArray(value)) {
+      // If type is array and values length less than number displayed
+      // join the values
+      if (multiple) {
+        if (value.length <= numberDisplayed) {
+          message = value
+            .map(opt => {
+              if (
+                typeof opt === 'object' &&
+                opt.hasOwnProperty(valueKey) &&
+                opt.hasOwnProperty(labelKey)
+              ) {
+                return opt[labelKey];
+              }
+              return opt;
+            })
+            .join(', ');
+        } else {
+          //If more than numberDisplayed then show "length selected"
+          message = `${value.length} selected`;
+        }
       } else {
-        //If more than numberDisplayed then show "length selected"
-        message = `${value.length} selected`;
+        if (
+          typeof value === 'object' &&
+          value.hasOwnProperty(valueKey) &&
+          value.hasOwnProperty(labelKey)
+        ) {
+          message = value[labelKey];
+        }
+        message = value[0].toString();
       }
     } else {
       if (
@@ -46,22 +65,13 @@ const Placeholder = ({
         value.hasOwnProperty(labelKey)
       ) {
         message = value[labelKey];
+      } else {
+        message = value;
       }
-      message = value[0].toString();
     }
-  } else {
-    if (
-      typeof value === 'object' &&
-      value.hasOwnProperty(valueKey) &&
-      value.hasOwnProperty(labelKey)
-    ) {
-      message = value[labelKey];
-    } else {
-      message = value;
-    }
+    return <span className="picky__placeholder">{message}</span>;
   }
-  return <span className="picky__placeholder">{message}</span>;
-};
+}
 
 Placeholder.defaultProps = {
   placeholder: 'None selected'
