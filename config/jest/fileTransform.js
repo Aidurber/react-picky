7d2e1ef67875474724fcb581386e2eef845c9d4e
Andrew Durber <aidurber@gmail.com>
2017-12-27 12:43:12 +0000

    build(linting): Removed dead code and added linting as a pretest

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    process(src, filename, config, options) {
-        return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
-    },
+  process(src, filename) {
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+  }
 };
