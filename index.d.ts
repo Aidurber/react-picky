1f1b894eb56bc0bbf0c7c62ddb20f62b74930b1a
Andrew Durber <aidurber@gmail.com>
2018-07-08 09:36:39 +0100

    fix(types): Addded missing typescript defs

--- index.d.ts
+++ index.d.ts
@@ -4,26 +4,31 @@ declare module 'react-picky' {
   export type PickyTabIndex = string | number;
 
   export interface PickyProps {
-    onChange: (...args: any[]) => any;
-    onFiltered?: (...args: any[]) => any;
-    onOpen?: (...args: any[]) => any;
-    onClose?: (...args: any[]) => any;
-    options: any[];
     placeholder?: string;
     value?: PickyValue;
     numberDisplayed?: number;
     multiple?: boolean;
+    options: any[];
+    onChange: (...args: any[]) => any;
     open?: boolean;
     includeSelectAll?: boolean;
     includeFilter?: boolean;
     filterDebounce?: number;
     dropdownHeight?: number;
+    onFiltered?: (...args: any[]) => any;
+    onOpen?: (...args: any[]) => any;
+    onClose?: (...args: any[]) => any;
     valueKey?: string;
     labelKey?: string;
     render?: (...args: any[]) => any;
-    itemHeight?: number;
     tabIndex?: PickyTabIndex;
     keepOpen?: boolean;
+    manySelectedPlaceholder?: string;
+    allSelectedPlaceholder?: string;
+    selectAllText?: string;
+    renderSelectAll: (...args: any[]) => any;
+    defaultFocusFilter?: boolean;
+    className?: string;
   }
 
   export default class Picky extends React.PureComponent<PickyProps, any> {
