6833c2176c5367c8fd8bb8e13b68fca358e68c97
Andrew Durber <aidurber@gmail.com>
2018-10-21 15:19:55 +0100

    refactor(Placeholder): Make placeholder a sfc

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -9,71 +9,64 @@ const isEmptyValue = value =>
   value === undefined ||
   (Array.isArray(value) && !value.length);
 
-class Placeholder extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {
-      placeholder,
-      value,
-      numberDisplayed,
-      multiple,
-      valueKey,
-      labelKey,
-      manySelectedPlaceholder,
-      allSelectedPlaceholder,
-      allSelected,
-    } = this.props;
-
-    let message = '';
-    if (isEmptyValue(this.props.value)) {
-      message = placeholder;
-    } else {
-      if (Array.isArray(value) && multiple) {
-        // If type is array and values length less than number displayed
-        // join the values
-        if (value.length <= numberDisplayed) {
-          message = value
-            .map(opt => {
-              if (isDataObject(opt, valueKey, labelKey)) {
-                return opt[labelKey];
-              }
-              return opt;
-            })
-            .join(', ');
-        } else {
-          // if many selected and not all selected then use the placeholder
-          if (manySelectedPlaceholder && !allSelected) {
-            // if it doesn't include the sprintf token then just use the placeholder
-            message = includes(manySelectedPlaceholder, '%s')
-              ? format(manySelectedPlaceholder, value.length)
-              : manySelectedPlaceholder;
-            //If all selected and there is an allselectedplaceholder use that
-          } else if (allSelected && allSelectedPlaceholder) {
-            // if it doesn't include the sprintf token then just use the placeholder
-            message = includes(allSelectedPlaceholder, '%s')
-              ? format(allSelectedPlaceholder, value.length)
-              : allSelectedPlaceholder;
-          }
-        }
+const Placeholder = ({
+  placeholder,
+  value,
+  numberDisplayed,
+  multiple,
+  valueKey,
+  labelKey,
+  manySelectedPlaceholder,
+  allSelectedPlaceholder,
+  allSelected,
+}) => {
+  let message = '';
+  if (isEmptyValue(value)) {
+    message = placeholder;
+  } else {
+    if (Array.isArray(value) && multiple) {
+      // If type is array and values length less than number displayed
+      // join the values
+      if (value.length <= numberDisplayed) {
+        message = value
+          .map(opt => {
+            if (isDataObject(opt, valueKey, labelKey)) {
+              return opt[labelKey];
+            }
+            return opt;
+          })
+          .join(', ');
       } else {
-        let tempValue = Array.isArray(value) ? value[0] : value;
-        if (isDataObject(tempValue, valueKey, labelKey)) {
-          message = tempValue[labelKey];
-        } else {
-          message = tempValue;
+        // if many selected and not all selected then use the placeholder
+        if (manySelectedPlaceholder && !allSelected) {
+          // if it doesn't include the sprintf token then just use the placeholder
+          message = includes(manySelectedPlaceholder, '%s')
+            ? format(manySelectedPlaceholder, value.length)
+            : manySelectedPlaceholder;
+          //If all selected and there is an allselectedplaceholder use that
+        } else if (allSelected && allSelectedPlaceholder) {
+          // if it doesn't include the sprintf token then just use the placeholder
+          message = includes(allSelectedPlaceholder, '%s')
+            ? format(allSelectedPlaceholder, value.length)
+            : allSelectedPlaceholder;
         }
       }
+    } else {
+      let tempValue = Array.isArray(value) ? value[0] : value;
+      if (isDataObject(tempValue, valueKey, labelKey)) {
+        message = tempValue[labelKey];
+      } else {
+        message = tempValue;
+      }
     }
-
-    return (
-      <span className="picky__placeholder" data-testid="picky_placeholder">
-        {message}
-      </span>
-    );
   }
-}
+
+  return (
+    <span className="picky__placeholder" data-testid="picky_placeholder">
+      {message}
+    </span>
+  );
+};
 
 Placeholder.defaultProps = {
   placeholder: 'None selected',
