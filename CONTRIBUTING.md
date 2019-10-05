7a0873e8c762e6a787314bcf4e97984c4c59d162
Andrew Durber <aidurber@gmail.com>
2019-10-05 12:37:18 +0100

    docs: add example sandbox project

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -9,39 +9,9 @@
 1. We use conventional commits to help generate change logs and manage releases. For example you're making a bug fix, you commit message would be: `fix: added aria-role to button`. This is enforced by [commitlint](https://commitlint.js.org/#/). For more information regarding conventional commits [see here](https://www.conventionalcommits.org/en/v1.0.0-beta.4/#summary)
 1. Create your Pull Request stating a reason for the PR and how your code resolves the issue.
 
-# Running the component locally
+# Running a sandbox
 
-It's on my todo list to add an example directory where the component lives and manual testing can be done. In the mean time you can follow these steps to manually test:
+1. Run `yarn prepare-example` - This will install the node_modules for the example project
+1. Run `yarn start`. This will start Rollup in watch mode and start the example project
 
-1. In the Picky directory run:
-
-```bash
-yarn link
-```
-
-This will create a [symbolic link](https://en.wikipedia.org/wiki/Symbolic_link) to Picky, we'll use that link in an upcoming step.
-
-2. Create a React app in another directory, I personally use Create React App and call it "sandbox".
-
-```bash
-yarn create react-app my-app
-```
-
-3. In the new sandbox project run
-
-```bash
-yarn link "react-picky"
-```
-
-4. You can copy & paste the contents of `index.js` from this [CodeSandbox](https://codesandbox.io/s/747z71zpxj) into the `App.tsx`
-5. In your terminal inside Picky run
-
-```bash
-yarn run dev
-```
-
-This will compile the package and watch for changes
-
-6. Start the Sandbox project, if you used Create React App you can run `yarn start`
-
-The code should be linked and any changes you make to Picky will be reflected in the sandbox.
+Any changes you make to Picky will be reflected in the sandbox, the typical URL for the example project is http://localhost:1234
---
+++ example/index.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Picky from '../src';
+import '../src/Picky.css';
+
+const bigList: any[] = [];
+
+for (var i = 1; i <= 200; i++) {
+  bigList.push({ id: i, name: `Item ${i}` });
+}
+class App extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      arrayValue: [],
+    };
+    this.selectOption = this.selectOption.bind(this);
+    this.selectMultipleOption = this.selectMultipleOption.bind(this);
+  }
+
+  selectOption(value) {
+    console.log('Vals', value);
+    this.setState({ value });
+  }
+  selectMultipleOption(value) {
+    console.log('Val', value);
+    this.setState({ arrayValue: value });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h3>Multi select</h3>
+            <Picky
+              value={this.state.arrayValue}
+              options={bigList}
+              onChange={this.selectMultipleOption}
+              open={true}
+              valueKey="id"
+              labelKey="name"
+              multiple={true}
+              includeSelectAll={true}
+              includeFilter={true}
+              dropdownHeight={600}
+            />
+          </div>
+          <div className="col">
+            <h3>Single select</h3>
+            <Picky
+              value={this.state.value}
+              options={bigList}
+              onChange={this.selectOption}
+              open={true}
+              valueKey="id"
+              labelKey="name"
+              multiple={false}
+              includeSelectAll={true}
+              includeFilter={true}
+              dropdownHeight={600}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
