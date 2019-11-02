7f1a5891fb0505dafa5930b7aaac12505519c77a
Andrew Durber <aidurber@gmail.com>
2019-11-02 09:22:55 +0000

    test: remove enzyme from placeholder

--- src/__tests__/Placeholder.test.tsx
+++ src/__tests__/Placeholder.test.tsx
@@ -1,48 +1,47 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import Placeholder from '../Placeholder';
-//@ts-ignore
-const sel = id => `[data-testid="${id}"]`;
-//@ts-ignore
-const getPlaceholderFromWrapper = wrapper =>
-  wrapper.find(sel('picky_placeholder'));
+
+const PLACEHOLDER_TESTID = 'picky_placeholder';
 
 describe('Placeholder', () => {
   it('should show placeholder if no initial values', () => {
-    //@ts-ignore
-    const wrapper = mount(<Placeholder placeholder="No selected values" />);
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-    expect(placeholder.text()).toEqual('No selected values');
+    const { getByTestId } = render(
+      //@ts-ignore
+      <Placeholder placeholder="No selected values" />
+    );
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('No selected values');
   });
 
   it('should default to None Selected if no placeholder supplied', () => {
-    //@ts-ignore
-    const wrapper = mount(<Placeholder />);
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-    expect(placeholder.text()).toEqual('None selected');
+    const { getByTestId } = render(
+      //@ts-ignore
+      <Placeholder />
+    );
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('None selected');
   });
 
   it('should show numberDisplayed if selected values', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder numberDisplayed={2} value={[1, 2]} multiple />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('1, 2');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('1, 2');
   });
   it('should cut off if more values than  numberDisplayed', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder numberDisplayed={2} value={[1, 2, 3]} multiple />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('3 selected');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('3 selected');
   });
 
   it('should use manySelectedPlaceholder if supplied and number of selected items greater than number displayed', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder
         numberDisplayed={2}
@@ -51,13 +50,13 @@ describe('Placeholder', () => {
         manySelectedPlaceholder="Thats %s selected"
       />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('Thats 3 selected');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('Thats 3 selected');
   });
 
   it('should use allSelectedPlaceholder prop if supplied and all items selected', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
+      //@ts-ignore
       <Placeholder
         numberDisplayed={2}
         value={[1, 2, 3]}
@@ -66,13 +65,13 @@ describe('Placeholder', () => {
         allSelectedPlaceholder="All selected"
       />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('All selected');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('All selected');
   });
 
   it('should x selected if all items selected and no allSelectedPlaceholder specified', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
+      //@ts-ignore
       <Placeholder
         numberDisplayed={2}
         value={[1, 2, 3]}
@@ -80,37 +79,37 @@ describe('Placeholder', () => {
         multiple
       />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('3 selected');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('3 selected');
   });
   it('should cut off if more values than  numberDisplayed', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder numberDisplayed={2} value={[1, 2, 3]} multiple />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('3 selected');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('3 selected');
   });
 
   it('should show value if not multiple and a value specified', () => {
-    //@ts-ignore
-    const wrapper = mount(<Placeholder value={'one'} />);
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('one');
+    const { getByTestId } = render(
+      //@ts-ignore
+      <Placeholder value={'one'} />
+    );
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('one');
   });
   it('should show first value if multiple value and not multiple set', () => {
-    //@ts-ignore
-    const wrapper = mount(<Placeholder value={[1, 2, 3]} />);
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('1');
+    const { getByTestId } = render(
+      //@ts-ignore
+      <Placeholder value={[1, 2, 3]} />
+    );
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('1');
   });
 
   it('should show label if object array supplied', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder
         value={[{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }]}
@@ -120,12 +119,11 @@ describe('Placeholder', () => {
         numberDisplayed={3}
       />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('Item 1, Item 2');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('Item 1, Item 2');
   });
   it('should show label if object supplied', () => {
-    const wrapper = mount(
+    const { getByTestId } = render(
       //@ts-ignore
       <Placeholder
         value={[{ id: 1, name: 'Item 1' }]}
@@ -134,8 +132,7 @@ describe('Placeholder', () => {
         numberDisplayed={3}
       />
     );
-    const placeholder = getPlaceholderFromWrapper(wrapper);
-
-    expect(placeholder.text()).toEqual('Item 1');
+    const placeholder = getByTestId(PLACEHOLDER_TESTID);
+    expect(placeholder.textContent).toEqual('Item 1');
   });
 });
