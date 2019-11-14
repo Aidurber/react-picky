cac1b1bdea90fd8594b76ae8a61bbc07e8036920
Andrew Durber <aidurber@gmail.com>
2019-11-14 16:11:59 +0000

    chore(deps): update deps

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  setupFiles: ['./tests/helpers/setup.js'],
+  transform: {
+    '.(ts|tsx)': require.resolve('ts-jest/dist'),
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx}'],
+  testURL: 'http://localhost',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
+};
