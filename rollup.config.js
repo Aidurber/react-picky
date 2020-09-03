e3f711563a1139a2295a4ea12b0b89ab057dc820
Andrew Durber <aidurber@gmail.com>
2020-09-03 20:23:19 +0100

    build: fix build

--- rollup.config.js
+++ rollup.config.js
@@ -5,19 +5,21 @@ import filesize from 'rollup-plugin-filesize';
 const name = require('./package.json').main.replace(/\.js$/, '');
 
 const ext = format =>
-  format == 'dts' ? 'd.ts' : format == 'cjs' ? 'js' : 'mjs';
+  format === 'dts' ? 'd.ts' : format === 'cjs' ? 'js' : 'mjs';
 
 const plugins = [css({ output: 'dist/picky.css' })];
 const bundle = format => ({
   input: 'src/index.ts',
   output: {
     file: `${name}.${ext(format)}`,
-    format: format == 'cjs' ? 'cjs' : 'es',
-    sourcemap: format != 'dts',
+    format: format === 'cjs' ? 'cjs' : 'es',
+    sourcemap: format !== 'dts',
   },
   plugins:
-    format == 'dts' ? [...plugins, dts()] : [...plugins, esbuild(), filesize()],
-  external: id => !/^[./]/.test(id),
+    format === 'dts'
+      ? [...plugins, dts()]
+      : [...plugins, esbuild(), filesize()],
+  external: ['react', 'react-dom'],
 });
 
 export default [bundle('es'), bundle('cjs'), bundle('dts')];
