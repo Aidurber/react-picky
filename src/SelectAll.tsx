7652668e9850ee0fc36127f3c34172e8dbff232e
Andrew Durber <aidurber@gmail.com>
2020-09-03 19:31:03 +0100

    fix: useRef instead of createRef

--- src/SelectAll.tsx
+++ src/SelectAll.tsx
@@ -10,64 +10,52 @@ type SelectAllProps = {
   toggleSelectAll(): void;
   visible: boolean;
 };
-const SelectAll: React.FC<SelectAllProps> = React.memo(
-  ({
-    tabIndex,
-    disabled,
-    allSelected,
-    id,
-    selectAllText,
-    toggleSelectAll,
-    visible,
-  }) => {
-    const checkboxRef = React.createRef<HTMLInputElement>();
-    React.useEffect(() => {
-      if (checkboxRef.current !== null) {
-        checkboxRef.current.indeterminate = allSelected === 'partial';
-      }
-    }, [allSelected]);
-
-    if (!visible) {
-      return null;
+const SelectAll: React.FunctionComponent<SelectAllProps> = ({
+  tabIndex,
+  disabled,
+  allSelected,
+  id,
+  selectAllText,
+  toggleSelectAll,
+  visible,
+}) => {
+  const checkboxRef = React.useRef<HTMLInputElement | null>(null);
+  React.useEffect(() => {
+    if (checkboxRef.current) {
+      checkboxRef.current.indeterminate = allSelected === 'partial';
     }
-    return (
-      <div
-        tabIndex={tabIndex}
-        role="option"
-        data-testid="selectall"
-        id={id + '-option-' + 'selectall'}
-        data-selectall="true"
-        aria-selected={allSelected === 'all'}
-        className={allSelected === 'all' ? 'option selected' : 'option'}
-        onClick={toggleSelectAll}
-        onKeyPress={toggleSelectAll}
-      >
-        <input
-          type="checkbox"
-          ref={checkboxRef}
-          readOnly
-          data-testid="selectall-checkbox"
-          tabIndex={-1}
-          checked={allSelected === 'all'}
-          aria-label="select all"
-          disabled={disabled}
-        />
-        <span data-testid="select-all-text">{selectAllText}</span>
-      </div>
-    );
-  },
-  areEqual
-);
+  }, [allSelected]);
+
+  if (!visible) {
+    return null;
+  }
+  return (
+    <div
+      tabIndex={tabIndex}
+      role="option"
+      data-testid="selectall"
+      id={id + '-option-' + 'selectall'}
+      data-selectall="true"
+      aria-selected={allSelected === 'all'}
+      className={allSelected === 'all' ? 'option selected' : 'option'}
+      onClick={toggleSelectAll}
+      onKeyPress={toggleSelectAll}
+    >
+      <input
+        type="checkbox"
+        ref={checkboxRef}
+        readOnly
+        data-testid="selectall-checkbox"
+        tabIndex={-1}
+        checked={allSelected === 'all'}
+        aria-label="select all"
+        disabled={disabled}
+      />
+      <span data-testid="select-all-text">{selectAllText}</span>
+    </div>
+  );
+};
 
 SelectAll.displayName = 'Picky(SelectAll)';
 
-function areEqual(prevProps: SelectAllProps, nextProps: SelectAllProps) {
-  return (
-    prevProps.tabIndex === nextProps.tabIndex &&
-    prevProps.disabled === nextProps.disabled &&
-    prevProps.allSelected === nextProps.allSelected &&
-    prevProps.selectAllText === nextProps.selectAllText &&
-    prevProps.visible === nextProps.visible
-  );
-}
 export { SelectAll };
